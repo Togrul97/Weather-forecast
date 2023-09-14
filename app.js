bd3fdf90223ea7c51106ad0684752a21822063ce
Togrul97 togrul-Dev <sultanzadetogrul@gmail.com>
2023-09-14 05:46:02 +0000

    start2

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`;
             async function myweather() {
-                let responce = await fetch(url)
-                let data = await responce.json()
+                let responce = await fetch(url);
+                let data = await responce.json();
                 $("#city").html(data.name);
                 $("#temp").html(data.main.temp);
                 $("#feels").html(data.main.feels_like);
@@ -21,10 +21,10 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=fe57130549e94791f490784ddc5b1ff8&units=metric`;
         async function myweather() {
-            let responce = await fetch(url)
-            let data = await responce.json()
+            let responce = await fetch(url);
+            let data = await responce.json();
             $("#city").html(data.name);
             $("#temp").html(data.main.temp);
             $("#feels").html(data.main.feels_like);
